b54050e17247d2dbf616bae08feb9b4e2d986c30
unknown <marcinoles87@gmail.com>
2022-11-16 21:32:03 +0100

    form add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  state = {  } 
+  state = { 
+    username : '' ,
+   } 
+
+   handleOnChange = (e) => {
+    console.log(e.target.type);
+
+   }
 
 
   render() { 
     return (
      <div className='App'>
-       Formularz React
+       <form>
+        <label> twoje imie
+          <input 
+          type="text" 
+          id="user" 
+          name="username" 
+          value={this.state.username}
+          onChange={this.handleOnChange}
+          ></input>
+        </label>
+       </form>
      </div>
     );
   }
