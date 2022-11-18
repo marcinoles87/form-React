b943faf16e095e5d1abece53b84b65576e5a14d6
unknown <marcinoles87@gmail.com>
2022-11-18 21:29:17 +0100

    css + input

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import './App.css';
 class App extends React.Component {
   state = { 
     username : '' ,
-   } 
+    email : ''
+    } 
 
    handleOnChange = (e) => {
     console.log(e.target.type);
 
+
+  const value = e.target.value;
+  this.setState( {
+    username : value ,
+  } )
    }
+
+  
 
 
   render() { 
     return (
      <div className='App'>
        <form>
-        <label> twoje imie
+        <label htmlFor='user'> 
+          Twoje imie
           <input 
           type="text" 
           id="user" 
@@ -25,6 +34,29 @@ class App extends React.Component {
           onChange={this.handleOnChange}
           ></input>
         </label>
+
+        <label htmlFor='email'> 
+          Twoj adres e-mail :
+          <input 
+          type="email" 
+          id="email" 
+          name="email" 
+          value={this.state.username}
+          onChange={this.handleOnChange}
+          ></input>
+        </label>
+
+        <label htmlFor='trip'> 
+          Wybierz wycieczke
+          <input 
+          type="text" 
+          id="trip" 
+          name="trip" 
+          value={this.state.username}
+          onChange={this.handleOnChange}
+          ></input>
+        </label>
+
        </form>
      </div>
     );
